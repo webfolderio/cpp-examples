34feb142e426d235632ca315fff2d929ff4809e9
webfolderio <support@webfolder.io>
2020-02-20 17:49:16 +0300

    Added before after test

--- google-test/main.cpp
+++ google-test/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "gtest/gtest.h"
 
+class MyEnvironment : public ::testing::Environment {
+public:
+    ~MyEnvironment() override {}
+
+    // Override this to define how to set up the environment.
+    void SetUp() override {
+        printf("start");
+    }
+
+    // Override this to define how to tear down the environment.
+    void TearDown() override {
+        printf("end");
+    }
+};
+
 int main(int argc, char** argv) {
+    ::testing::AddGlobalTestEnvironment(new MyEnvironment);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
